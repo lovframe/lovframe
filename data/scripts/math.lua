6ec9e593ce1a7cee21409c874f6c23c00c5e474e
TriangleDevv <chandu.lanka13@gmail.com>
2021-12-05 19:26:45 +0530

    working on the math module... simple functions rn will add more when i need them

---
+++ data/scripts/math.lua
@@ -0,0 +1,13 @@
+math = {}
+
+function math:vector2(x,y)
+    return x, y
+end
+
+function math:round(number)
+    return math.floor(number+0.5)
+end
+
+function math:abs(number)
+    return math.abs(number)
+end
