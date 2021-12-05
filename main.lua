741ae6a9b75464bdf5f7e766fe4de66b77c87a19
TriangleDevv <chandu.lanka13@gmail.com>
2021-12-05 19:19:47 +0530

    created the logo for the framework and drawn it on the

--- main.lua
+++ main.lua
@@ -1,8 +1,11 @@
 function love.load()
+    logo = love.graphics.newImage("data/images/lovframe-logo.png")
+    love.graphics.setDefaultFilter("nearest", "nearest")
 end
 
 function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.draw(logo, 150, 100)
 end
